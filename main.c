412547006d4fa760db244b7d46701a9ef045dd9a
Frozzie <yasha.petrukhin@gmail.com>
2024-05-17 20:30:21 +0300

    Task 2, 3 done

--- main.c
+++ main.c
@@ -34,13 +34,183 @@ void Task1 (size_t n, query_t *query, size_t qn)
     }
 
     outputMatrix(&mat);
+    freeMemMatrix(&mat);
+    free(a);
 }
 
-int main() 
+void Task2 (size_t max_row, size_t max_col, int *a)
+{
+    matrix mat, mat_answ;
+    mat = createMatrixFromArray(a, max_row, max_col);
+    mat_answ = createMatrixFromArray(a, max_row, max_col);
+
+    for(size_t row = 0; row < max_row; row++)
+    {
+        for(size_t col = 0; col < max_col; col++)
+        {
+            int sum_of_living_near = 0;
+            
+            int sur_col, sur_row;
+            
+            sur_row = row - 1;
+            sur_col = col - 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+
+            sur_row = row - 1;
+            sur_col = col;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row - 1;
+            sur_col = col + 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row;
+            sur_col = col - 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row;
+            sur_col = col + 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row + 1;
+            sur_col = col - 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row + 1;
+            sur_col = col;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            sur_row = row + 1;
+            sur_col = col + 1;
+            if(sur_col >= 0 && sur_row >= 0 && sur_col < max_col && sur_row < max_row)
+            {
+                sum_of_living_near += getElementMatrix(&mat, sur_row, sur_col);
+            }
+            
+            if(getElementMatrix(&mat, row, col) == 1)
+            {
+                if(sum_of_living_near < 2 || sum_of_living_near > 3)
+                {
+                    putElementMatrix(&mat_answ, row, col, 0);
+                }
+            }
+            else
+            {
+                if(sum_of_living_near == 3)
+                {
+                    putElementMatrix(&mat_answ, row, col, 1);
+                }
+            }
+        }
+    }
+
+    outputMatrix(&mat_answ);
+
+    freeMemMatrix(&mat);
+    freeMemMatrix(&mat_answ);
+}
+
+void shellSort (int *a, size_t n)
+{
+    for (int s = n / 2; s > 0; s /= 2) 
+    {
+        for (int i = s; i < n; ++i) 
+        {
+            for (int j = i - s; j >= 0 && a[j] > a[j + s]; j -= s) 
+            {
+                int temp = a[j];
+                a[j] = a[j + s];
+                a[j + s] = temp;
+            }
+        }
+    }
+}
+
+int median (int *arr, size_t n)
+{
+    shellSort(arr, n);
+
+    return arr[n/2];
+}
+
+void Task3 (size_t n, matrix mat)
+{
+    if(n % 2 != 0)
+    {
+        int border = n / 2;
+        int *arr = malloc(sizeof(int) * n * n);
+
+        for(int i = border; i < mat.nRows - border; i++)
+        {
+            for(int j = border; j < mat.nCols - border; j++)
+            {   
+                int count = 0;
+                for(int k = i - border; k <= i + border; k++)
+                {
+                    for(int s = j - border; s <= j + border; s++)
+                    {
+                        arr[count++] = getElementMatrix(&mat, k, s);
+                    }
+                }
+                int med = median(arr, n * n);
+                putElementMatrix(&mat, i, j, med);
+            }
+        }
+    
+        free(arr);
+    }
+
+    outputMatrix(&mat);
+}
+
+int main()
 {
     int n = 3, qn = 2;
-
+    int a[] = {
+        0,1,0,
+        0,0,1,
+        1,1,1,
+        0,0,0
+    };
     query_t query[2] = {{1,1,2,2},{0,0,1,1}};
     
+    int b[] = {
+        10, 20, 30,
+        25, 35, 45,
+        15, 25, 35
+    };
+
+    matrix mat1 = createMatrixFromArray(b, 3, 3);
+    
     Task1(n, query, qn);
+    printf("\n");
+
+    Task2(4, 3, a);
+    printf("\n");
+
+    Task3(3, mat1);
+
+
+    freeMemMatrix(&mat1);
 }
